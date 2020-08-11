5ec76e21cc4f120a0b0627f6a494ed57c090a85f
adiweisberg <adi.weisberg5@gmail.com>
2020-08-11 15:21:34 +0300

    Update create_coordinaite_sys.py
    finding the widthes - completed V

--- create_coordinaite_sys.py
+++ create_coordinaite_sys.py
@@ -1,32 +1,77 @@
-import numpy
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import find_borders
+import math
+
 """
 1. finding width of the coordinaite:
 """
 #Find an equation straight to the muscle line you tagged
-#by the first and the last point we finde in find_borfers
+#by the first and the last point we found in find_borders
 #return line object
-#TODO BY: ADI
 def Finding_Equation_Line(point1, point2):
-    pass
+    point1 = np.array(point1)
+    print(point1)
+    point2 = np.array(point2)
+    print(point2)
+    z = np.polyfit(point1, point2, 1)
+    m, b = z
+    print(m, b)
+    line_length = np.poly1d(z)
+    plt.plot(point1, m*point1 + b)
+    plt.show()
+    return line_length
 
-#finding the normal of the muscle line for the nipple line.
-#finde the Straight slope and by that the nomal.
-#return the normal number.
-#TODO BY: MICHAL
-def Finding_Normal(line):
-    pass
-#for the nipple line:
-#return line
-#TODO BY: MICHAL
-def Finding_Equation_Line_By_Slope_And_Point(slope,point):
-    pass
+
+
+# finding the normal of the muscle line for the nipple line.
+# find the the slope of the line of the muscale(m1) and then calculate the normal:
+# m2 = -1 / m1.
+# return the normal number.
+def Finding_Normal(line, point1 = [1,2]):
+    x = np.array(point1)
+    m, b = line
+    m_normal = -1 / m
+    print(m_normal)
+    plt.plot(x, m_normal * x + b)
+    plt.show()
+    return m_normal
+
+# for the nipple line:
+# circle - get the circle ( the circle taf of the nipple )
+# slope - the number we get from the function "finding normal" that calculates the slope
+# y-y1 = m2(x-x1)
+# return line
+def Finding_Equation_Line_By_Slope_And_Point(slope):
+    x, y, r = center_point
+    m = slope
+    b = y - slope * x
+    line_width = np.poly1d((m, b))
+    return line_width
 
 
 #by Intersection Point betweeen line nipple and line muscle.
 #return width number
-#TODO BY: ADI
-def Finding_width(lineNipple,lineMuscle):
-    pass
+# x = (b2 - b1) / (m1 - m2)
+# put x in some equation and got y
+def Find_intercept_width_length(line_length, line_width):
+    m1, b1 = line_length
+    m2, b2 = line_width
+    x = (b2 - b1) / (m1 - m2)
+    y = m1 * x + b1
+    return (x, y)
+
+# This function gets 2 points of the nipple and the iterception point with the
+# length and width equation lines.
+# the function returns the width in float.
+def Find_Width(point1, point2):
+    x1, y1 = point1
+    x2, y2 = point2
+    # Calculating distance
+    return math.sqrt(math.pow(x2 - x1, 2) +
+                     math.pow(y2 - y1, 2) * 1.0)
+
 
 """
 2. finding Length
@@ -55,3 +100,27 @@ and then calculate for each array its AVG.
 """
 def calculate_Lengths_and_widths_avg():
     pass
+
+
+# Read an image
+image = cv2.imread("1-1.png")
+line_detected = np.copy(image)
+top_line, buttom_line = find_borders.findLine(image)
+cv2.line(line_detected, top_line, buttom_line, (208, 216, 75), 15)  # draw and show line between 2 points that we found
+cv2.namedWindow('line_detected', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('line_detected', 600, 600)
+cv2.imshow('line_detected', line_detected)
+cv2.waitKey()
+cv2.destroyAllWindows()
+output, circle, center_point = find_borders.fineCircle(image)
+print(circle)
+cv2.namedWindow('output1', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('output1', 600, 600)
+cv2.imshow("output1", output)
+cv2.waitKey(0)
+
+#################################
+eq_line_length = Finding_Equation_Line(top_line, buttom_line)
+normal = Finding_Normal(eq_line_length)
+eq_line_width = Finding_Equation_Line_By_Slope_And_Point(normal)
+intercept_width_length = Find_intercept_width_length(eq_line_length, eq_line_width)
