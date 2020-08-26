49d049316f0eeb69fdf5cad2be73047acc0563fc
adiweisberg <adi.weisberg5@gmail.com>
2020-08-26 16:28:00 +0300

    Update find_countor.py

--- find_countor.py
+++ find_countor.py
@@ -10,11 +10,12 @@ def getEdgeImage(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # Find Canny edges
     #edged = cv2.Canny(gray, 200, 200)
-    _, binary = cv2.threshold(gray, 30, 50, cv2.THRESH_BINARY_INV)
+    _, binary = cv2.threshold(gray, 23, 25, cv2.THRESH_BINARY_INV)
     contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     # draw all contours
     image = cv2.drawContours(image, contours, -1, (0, 255, 0), 5, cv2.LINE_AA)
     return contours,image
+
 
 def imageShowWithWait(edgeImage, image):
     #cv2.namedWindow(edgeImage, cv2.WINDOW_NORMAL)
@@ -29,12 +30,21 @@ def imageShowWithWait(edgeImage, image):
 #    ImageOps.crop(image, border)
 
 def main():
-    originalImage = cv2.imread('1-1-2.png')
+    originalImage = cv2.imread('1-1.png')
     #cut_annotations(originalImage)
     cv2.waitKey(0)
     contours, edgeImage = getEdgeImage(originalImage)
     imageShowWithWait("edgeImage", edgeImage)
-    print(contours)
-
+    #print(contours)
+    #listX, listY = zip(*contours[0])
+    #degree = 4
+    #weights = np.polyfit(listX, listY, degree)
+    #poly_equation = np.poly1d(weights)
+    #print(poly_equation)
+    #print(approx)
+    cv2.namedWindow("originalImage", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("originalImage", 600, 600)
+    cv2.imshow("originalImage", edgeImage)
+    cv2.waitKey(0)
 
 main()
