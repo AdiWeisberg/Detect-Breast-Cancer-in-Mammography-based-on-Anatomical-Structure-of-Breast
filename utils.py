d1e420a85a04773fb2d98afd463b26b5a5333173
adiweisberg <adi.weisberg5@gmail.com>
2020-10-12 16:54:06 +0300

    merge with flip and fix iteration over folders

--- utils.py
+++ utils.py
@@ -2,30 +2,43 @@ import cv2
 import os
 from image_procesor import run_processing
 from image_procesor import change_image_size, ratio_calc, sum_calc
+from image_format import *
 
-
-def image_size_locator(path_list):
+def image_size_locator(start_path):
     folder_lst = []
+    path_list = os.listdir(start_path)
 
-    for folder_name, path in path_list:
-        all_images_specific_folder = os.listdir(path)
+    for calc_mass_folders in path_list:
+        mid_path = os.path.join(start_path, calc_mass_folders)
+        images_folders = os.listdir(mid_path)
+        for num_folder in images_folders:
+            full_path = os.path.join(start_path, calc_mass_folders, num_folder)
+            source_path = full_path + "\\" + "source.png"
 
-        source = cv2.imread(path + "\\" + all_images_specific_folder[0])
-        tagged = cv2.imread(path + "\\" + all_images_specific_folder[1])
+            dcm_to_png(full_path)
+            source = cv2.imread(source_path)
+               
+            # Flip if the source is not at the right direction
+            if is_flip_to_left(source):
+                source = cv2.flip(source, 1)  # flip the image that the brest will be in the left side
+            tagged = cv2.imread(full_path + "\\" + "1-1.png")
 
-        shifted_image, image_info = run_processing(source, tagged, path)
-        folder_lst.append(image_info)
+            shifted_image, image_info = run_processing(source, tagged, full_path)
+            folder_lst.append(image_info)
     return folder_lst
 
 
-def image_resize_by_ratio(path_list, list_new, x_max, y_max):
-    for folder_name, path in path_list:
-        index = 0
-        all_images_specific_folder = os.listdir(path)
-
-        source = cv2.imread(path + "\\" + all_images_specific_folder[0])
-        change_image_size(source, path, index, list_new, x_max, y_max)
-        index = index + 1
+def image_resize_by_ratio(start_path, list_new, x_max, y_max):
+    path_list = os.listdir(start_path)
+    for calc_mass_folders in path_list:
+        mid_path = os.path.join(start_path, calc_mass_folders)
+        images_folders = os.listdir(mid_path)
+        for num_folder in images_folders:
+            index = 0
+            full_path = os.path.join(start_path, calc_mass_folders, num_folder)
+            source = cv2.imread(full_path + "\\" + "source.png")
+            change_image_size(source, full_path, index, list_new, x_max, y_max)
+            index = index + 1
 
 
 def calculate_image_ratio(sum_lst):
@@ -34,3 +47,4 @@ def calculate_image_ratio(sum_lst):
 
 def calculate_whole_image_size(image_info_lst):
     return sum_calc(image_info_lst)
+
