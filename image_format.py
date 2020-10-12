d2d8f66452519d7e8021b4c1c8d671727d4af471
adiweisberg <adi.weisberg5@gmail.com>
2020-10-12 16:58:16 +0300

    fix iteration on folders

--- image_format.py
+++ image_format.py
@@ -1,17 +1,23 @@
+import os
 import pydicom as dicom
 import cv2
 
 
 def dcm_to_png(full_path):
     try:
-        dcm_to_png_file = full_path + "\\1-1.dcm"
+        dcm_to_png_file = full_path + "\\" + "1-1.dcm"
         ds = dicom.dcmread(dcm_to_png_file)
-        png_image = ds.pixel_array
-        # save_path = full_path +"\\" + "source.png"
-        # print("dcm to png for file " + str(n) + " succeeded? ", str(cv2.imwrite(save_path, pixel_array_numpy)))
-        return png_image
+        pixel_array_numpy = ds.pixel_array
+        save_path = full_path +"\\" + "source.png"
+        print("dcm to png for file succeeded? ", str(cv2.imwrite(save_path, pixel_array_numpy)))
+        
+        # Delete 1-1.dcm file 
+        del_file = full_path +"\\" + "1-1.dcm"
+        if os.path.exists(del_file):
+            os.remove(del_file)
+
     except OSError as e:
-        print("error")
+        print("error!")
 
 
 def is_flip_to_left(png_image):
@@ -25,7 +31,7 @@ If the sum of color on the right side is higher on the left side it means
     height, width = png_image.shape[:2]
     width_half = width // 2
 
-    ret, thresh_img = cv2.threshold(png_image, 127, 255, cv2.THRESH_BINARY)
+    ret, thresh_img = cv2.threshold(png_image, 30, 255, cv2.THRESH_BINARY)
     right = (thresh_img[0:height, width_half:width] // 250).sum()
     left = (thresh_img[0:height, 0:width_half] // 250).sum()
 
