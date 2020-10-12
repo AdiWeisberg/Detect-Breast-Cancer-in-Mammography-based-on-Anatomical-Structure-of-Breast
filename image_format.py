9d2f33b9cb43e3ebb31636ec840b1fc4622b9285
Michal Yelkin <44944939+michalkin9@users.noreply.github.com>
2020-10-12 13:45:07 +0300

    change format and flip

---
+++ image_format.py
@@ -0,0 +1,35 @@
+import pydicom as dicom
+import cv2
+
+
+def dcm_to_png(full_path):
+    try:
+        dcm_to_png_file = full_path + "\\1-1.dcm"
+        ds = dicom.dcmread(dcm_to_png_file)
+        png_image = ds.pixel_array
+        # save_path = full_path +"\\" + "source.png"
+        # print("dcm to png for file " + str(n) + " succeeded? ", str(cv2.imwrite(save_path, pixel_array_numpy)))
+        return png_image
+    except OSError as e:
+        print("error")
+
+
+def is_flip_to_left(png_image):
+    """
+Equates the threshing of each side in the image.
+If the sum of color on the right side is higher on the left side it means
+ there is more white there, so the breast is on the right side
+    :param img:cv2 image
+    :return:boolean- true if the breast is in the right side.
+    """
+    height, width = png_image.shape[:2]
+    width_half = width // 2
+
+    ret, thresh_img = cv2.threshold(png_image, 127, 255, cv2.THRESH_BINARY)
+    right = (thresh_img[0:height, width_half:width] // 250).sum()
+    left = (thresh_img[0:height, 0:width_half] // 250).sum()
+
+    if left < right:
+        return True
+    else:
+        return False
