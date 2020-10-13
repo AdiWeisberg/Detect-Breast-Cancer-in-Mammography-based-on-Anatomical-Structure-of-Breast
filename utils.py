b656ed64c6e813e1b051dc13aeff8157ed68383d
adiweisberg <adi.weisberg5@gmail.com>
2020-10-13 08:55:49 +0300

    last version

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import os
 from image_processor import run_processing
 from image_processor import change_image_size, ratio_calc, sum_calc
 from image_format import *
+from image_processor import draw_roi
 
 def image_size_locator(start_path):
     folder_lst = []
@@ -13,19 +14,27 @@ def image_size_locator(start_path):
         images_folders = os.listdir(mid_path)
         for num_folder in images_folders:
             full_path = os.path.join(start_path, calc_mass_folders, num_folder)
-            source_path = full_path + "\\" + "source.png"
+            mask = cv2.imread(full_path + "\\" + "mask.png", cv2.IMREAD_GRAYSCALE)
 
+            # Convert dcm to png:
             dcm_to_png(full_path)
-            source = cv2.imread(source_path)
-               
-            # Flip if the source is not at the right direction
+            source = cv2.imread(full_path + "\\" + "source.png")
+
+            # flip the image so that the brest will be in the left side if needed
             if is_flip_to_left(source):
-                source = cv2.flip(source, 1)  # flip the image that the brest will be in the left side
+                source = cv2.flip(source, 1)
+                cv2.imwrite(full_path + "\\" + "source.png", source)
             tagged = cv2.imread(full_path + "\\" + "1-1.png")
 
-            shifted_image, image_info = run_processing(source, tagged, full_path)
+            # Run image processing, shifted_image is the result and shifted_info is the measurements of the breast and new image
+            shifted_image, image_info = run_processing(source, tagged, full_path, mask)
+
             folder_lst.append(image_info)
     return folder_lst
+
+
+def calculate_image_ratio(sum_lst):
+    return ratio_calc(sum_lst)
 
 
 def image_resize_by_ratio(start_path, list_new, x_max, y_max):
@@ -33,17 +42,13 @@ def image_resize_by_ratio(start_path, list_new, x_max, y_max):
     for calc_mass_folders in path_list:
         mid_path = os.path.join(start_path, calc_mass_folders)
         images_folders = os.listdir(mid_path)
-        for num_folder in images_folders:
-            index = 0
+        for index, num_folder in enumerate(images_folders):
             full_path = os.path.join(start_path, calc_mass_folders, num_folder)
             source = cv2.imread(full_path + "\\" + "source.png")
-            change_image_size(source, full_path, index, list_new, x_max, y_max)
-            index = index + 1
-
-
-def calculate_image_ratio(sum_lst):
-    return ratio_calc(sum_lst)
-
+            mask = cv2.imread(full_path + "\\" + "mask_cropped.png")
+            change_image_size("final_ratio.png", source, full_path, index, list_new, x_max, y_max)
+            change_image_size("final_mask.png", mask, full_path, index, list_new, x_max, y_max)
+            draw_roi(full_path, "final_ratio.png", "final_mask.png", "final_roi.png")
 
 def calculate_whole_image_size(image_info_lst):
     return sum_calc(image_info_lst)
